a3d0b4514c2bc108a8130eb50ccf7ec08a4bfebd
Tomo Ferega <tomo@element.hr>
2013-07-27 14:07:04 +0200

    User will now be redirected to auth page if not logged in. Added "isLoggedIn" mock to User.

--- code/src/main/scala/com/ferega/todo/lift/Boot.scala
+++ code/src/main/scala/com/ferega/todo/lift/Boot.scala
@@ -1,21 +1,27 @@
 package com.ferega.todo
 package lift
 
-import net.liftweb.sitemap.Loc.Hidden
-import net.liftweb.http.LiftRules
+import net.liftweb.common.{ Empty, Full }
+import net.liftweb.http.{ Bootable, Html5Properties, LiftRules }
+import net.liftweb.http.{ Req, S }
+import net.liftweb.sitemap.Loc.{ EarlyResponse, Hidden }
 import net.liftweb.sitemap.{ Menu, SiteMap }
-import net.liftweb.http.LiftRulesMocker.toLiftRules
-import net.liftweb.sitemap.LocPath.stringToLocPath
-import net.liftweb.http.Req
-import net.liftweb.http.Html5Properties
-import net.liftweb.util.Vendor.valToVender
-import net.liftweb.http.Bootable
 
 class Boot extends Bootable {
+  def loggedIn =
+    () => {
+      User.isLoggedIn match {
+        case true => Empty
+        case false => {
+          Full(S.redirectTo("/auth"))
+        }
+      }
+    }
+
   def boot() {
     LiftRules.addToPackages("com.ferega.todo.lift")
     LiftRules.setSiteMap(SiteMap(
-        Menu("Home") / "index" >> Hidden,
+        Menu("Home") / "index" >> Hidden >> EarlyResponse(loggedIn),
         Menu("Auth") / "auth" >> Hidden)
     )
 
--- code/src/main/scala/com/ferega/todo/lift/User.scala
+++ code/src/main/scala/com/ferega/todo/lift/User.scala
@@ -3,4 +3,6 @@ package com.ferega.todo.lift
 object User {
   def logIn(username: String, password: String): Either[String, Unit] = Left("Not implemented")
   def create(username: String, password: String): Either[String, Unit] = Left("Not implemented")
+
+  def isLoggedIn = false
 }
